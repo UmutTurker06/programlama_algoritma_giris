1ece19ab1880ade892af85c643f525a1eb3f8acf
Umut Türker <147622987+UmutTurker06@users.noreply.github.com>
2023-10-13 01:27:56 +0300

    Revert "Soru_10_11_12_çözüm"

--- 10_dosyalama_uyg/bolum_sonu_sorular/soru11.py
+++
@@ -1,6 +0,0 @@
-#Soru 11: Bir Bölümdeki Öğrencilerin Notlarını Tutan Dosya Uygulaması Programını Geliştiriniz
-dosya =open('notlar.txt','a')
-ad = input("Öğrenci Adı-Soyadı :")
-no = input("Öğrencinin Notu :")
-dosya.write(ad+"\t"+no+"\n")
-dosya.close()
--- 10_dosyalama_uyg/bolum_sonu_sorular/soru12.py
+++
@@ -1,7 +0,0 @@
-#Soru 12 : Bir Mağazadaki Ürünlerin Bilgisini Tutan Dosya Uygulamasını Geliştiriniz 
-dosya =open('ürün.txt', 'a')
-ad = input("Ürün Adını Giriniz : ")
-cins = input("Ürünün Cinsini Giriniz : ")
-fiyat = input("Ürünün Fiyatını Giriniz : ")
-dosya.write(ad+"\t"+cins+"\t"+fiyat+"\n")
-dosya.close()
